f4763d231e00ba9da05413142c75ce0cc0fbbc2b
Hasan GÖKALP <Hasan GKALP@DESKTOP-R04PVQ3>
2023-10-04 11:45:24 +0300

    Changes

--- GokalpStock.Application/Concrete/Service/BillingService.cs
+++ GokalpStock.Application/Concrete/Service/BillingService.cs
@@ -1,13 +1,24 @@
-﻿using GokalpStock.Application.Abstract.Service;
+﻿using AutoMapper;
+using GokalpStock.Application.Abstract.Service;
 using GokalpStock.Application.Concrete.Models.Dtos;
 using GokalpStock.Application.Concrete.Models.RequestModels.Billings;
 using GokalpStock.Application.Concrete.Wrapper;
+using GokalpStock.Persistence.Abstract.UnitWork;
 using System.Linq.Expressions;
 
 namespace GokalpStock.Application.Concrete.Service
 {
     public class BillingService : IBillingService
     {
+        private readonly IMapper _mapper;
+        private readonly IUnitWork _unitWork;
+
+        public BillingService(IMapper mapper, IUnitWork unitWork)
+        {
+            _mapper = mapper;
+            _unitWork = unitWork;
+        }
+
         public Result<bool> CreateBilling(CreateBillingsRM createBillingsRM)
         {
             throw new NotImplementedException();
--- GokalpStock.Application/Concrete/Service/ProductService.cs
+++ GokalpStock.Application/Concrete/Service/ProductService.cs
@@ -1,13 +1,24 @@
-﻿using GokalpStock.Application.Abstract.Service;
+﻿using AutoMapper;
+using GokalpStock.Application.Abstract.Service;
 using GokalpStock.Application.Concrete.Models.Dtos;
 using GokalpStock.Application.Concrete.Models.RequestModels.Products;
 using GokalpStock.Application.Concrete.Wrapper;
+using GokalpStock.Persistence.Abstract.UnitWork;
 using System.Linq.Expressions;
 
 namespace GokalpStock.Application.Concrete.Service
 {
     public class ProductService : IProductService
     {
+        private readonly IMapper _mapper;
+        private readonly IUnitWork _unitWork;
+
+        public ProductService(IMapper mapper, IUnitWork unitWork)
+        {
+            _mapper = mapper;
+            _unitWork = unitWork;
+        }
+
         public Result<bool> CreateProduct(CreateProductRM createProductsRM)
         {
             throw new NotImplementedException();
