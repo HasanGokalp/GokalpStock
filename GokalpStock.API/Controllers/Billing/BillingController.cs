88b6682884e9d88231ebeabf932e309d149ddb84
Hasan GÖKALP <Hasan GKALP@DESKTOP-R04PVQ3>
2023-10-05 16:17:45 +0300

    Changes

---
+++ GokalpStock.API/Controllers/Billing/BillingController.cs
@@ -0,0 +1,44 @@
+﻿using GokalpStock.Application.Abstract.Service;
+using GokalpStock.Application.Concrete.Models.Dtos;
+using GokalpStock.Application.Concrete.Models.RequestModels.Billings;
+using GokalpStock.Application.Concrete.Wrapper;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Crypto.Prng.Drbg;
+
+namespace GokalpStock.API.Controllers.Billing
+{
+    [Route("Billing")]
+    public class BillingController : Controller
+    {
+        private readonly IHomeService _homeService;
+        public BillingController(IHomeService homeService)
+        {
+            _homeService = homeService;
+        }
+        [HttpGet("GetAllBillings")]
+        public ActionResult<Result<BillingDto>> GetAllBillings()
+        {
+            var entities = _homeService.BillingService.GetAllBilling();
+            return Ok(entities);
+        }
+        [HttpPost("CreateBilling")]
+        public ActionResult<Result<bool>> CreateBilling(CreateBillingsRM createBillingsRM)
+        {
+            var result = _homeService.BillingService.CreateBilling(createBillingsRM);
+            return Ok(result);
+        }
+        [HttpPost("DeleteBilling")]
+        public ActionResult<Result<bool>> DeleteBilling(DeleteBillingRM deleteBillingsRM)
+        {
+            var result = _homeService.BillingService.DeleteBilling(deleteBillingsRM);
+            return Ok(result);
+        }
+        [HttpPost("UpdateBilling")]
+        public ActionResult<Result<bool>> UpdateBilling(UpdateBillingRM updateBillingRM)
+        {
+            var result = _homeService.BillingService.UpdateBilling(updateBillingRM);
+            return Ok(result);
+        }
+
+    }
+}
